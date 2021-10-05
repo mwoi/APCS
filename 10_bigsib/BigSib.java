378131d00497b5af5d3ba6cfc1a8b6ec0ecf3e63
Ives Wu <mwu20@stuy.edu>
2021-10-05 00:31:11 -0400

    initsetup

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,19 @@
+/* Marcus Wu
+   APCS
+   HW10 -- Refactor Big Sib One
+   2021-10-05
+
+   DISCOVERIES
+   	--
+   UNRESOLVED QUESTIONS
+   	--
+*/
+
+
+public class BigSib{
+	public static String greet(String name){
+		return ("Hello, " + name +  ". Hello, thank-you, and hello.");
+	}
+}
+
+
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,19 @@
+/* Marcus Wu
+   APCS
+   HW08 -- Refactor Big Sib One
+   2021-10-05
+
+   DISCOVERIES
+   	--
+   UNRESOLVED QUESTIONS
+   	--
+*/
+
+
+public class Greet{
+	public static void main(String[] args){
+		System.out.println( BigSib.greet("River") );
+		System.out.println( BigSib.greet("Vanjani") );
+		System.out.println( BigSib.greet("Yagupsky") );
+	}
+}
