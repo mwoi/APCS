72664b47b86804be0182eb719ac3615c0215bf2a
Ives Wu <mwu20@stuy.edu>
2021-10-27 02:46:42 -0400

    25_repeat

--- 25_repeat/Repeat.java
+++ 25_repeat/Repeat.java
@@ -1,19 +1,20 @@
 /*
-Nina Jiang
+Nina Jiang + Marcus Wu + Kevin Cheng
 APCS
-2021-10-26
-time spent: 0.53h
+HW25 -- Do I repeat myself?
+2021-10-27
+time spent: 0.7
 DISCO: n/a
 Q/C/C: n/a
 */
 
 public class Repeat {
 	public static String fenceW(int numPosts) {
-		int x = 1;
+		int x = numPosts;
 		String result = "|";
-		while (x < numPosts) {
+		while (1 < x) {
 			result += "--|";
-			x++;
+			x--;
 		}
 		return result;
 	}
