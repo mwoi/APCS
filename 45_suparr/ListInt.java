43b0a8e266381c11354af53a491bb80bef87ad53
marcuswu <90876277+mwoi@users.noreply.github.com>
2021-12-09 02:25:13 -0500

    hw45

---
+++ 45_suparr/ListInt.java
@@ -0,0 +1,9 @@
+public interface ListInt {
+
+	public boolean add( int x );
+	public boolean add( int index, int val );
+	public int get( int index );
+	public int set( int index, int val );
+	public int size();
+
+}
