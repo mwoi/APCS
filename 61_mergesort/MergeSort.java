c60f4b9b78c31bbead4aa7d187872b70920c6c4f
marcuswu <90876277+mwoi@users.noreply.github.com>
2022-02-08 23:35:42 -0500

    hw61 work

--- 61_mergesort/MergeSort.java
+++ 61_mergesort/MergeSort.java
@@ -1,10 +1,20 @@
+/*
+Team Turtle (Marcus Wu and Ivina Wang)
+APCS pd7
+HW#61 -- Instructions so Simple...
+2022-02-08
+time spent: 1 hr
+*/
 /***
   class MergeSort
   Implements mergesort on array of ints.
 
   Summary of Algorithm:
-
-  ***/
+    You split the array in half until the half arrays have only
+      1 element
+    Then merge the arrays maintaining ascending order, until you are left with
+      1 sorted array
+***/
 
 public class MergeSort
 {
@@ -17,27 +27,28 @@ public class MergeSort
    ******************************************************/
   private static int[] merge( int[] a, int[] b )
   {
-	int[] output = new int[ ( a.length + b.length ) ];
-	int checkA = 0;
-	int checkB = 0;
 
-	for ( int i = 0; i < output.length - 1; i++ ) {
-		if ( checkA == ( a.length - 1 ) ) {
-			output[i] = b[checkB];
-			checkB++;
-		} else if ( checkB == ( b.length - 1 ) ) {
-			output[i] = a[checkA];
-			checkA++;
-		} else {
-			if ( a[checkA] < b[checkB] ) {
-				output[i] = a[checkA];
-				checkA++;
-			} else {
-				output[i] = b[checkB];
-				checkB++;
-			}
-		}
-	}
+    int[] output = new int[ ( a.length + b.length ) ];
+  	int checkA = 0;
+  	int checkB = 0;
+
+  	for ( int i = 0; i < output.length ; i++ ) {
+  		if ( checkA == a.length ) { //if done checking array a
+  			output[i] = b[checkB];
+  			checkB++;
+  		} else if ( checkB == b.length ) { //if done checking array b
+  			output[i] = a[checkA];
+  			checkA++;
+  		} else {
+  			if ( a[checkA] < b[checkB] ) { //if a < b, add a to output
+  				output[i] = a[checkA];
+  				checkA++;
+  			} else { //if b < a or b == a, add b to output
+  				output[i] = b[checkB];
+  				checkB++;
+  			}
+  		}
+  	}
 
 	return output;
   }//end merge()
@@ -50,7 +61,20 @@ public class MergeSort
    ******************************************************/
   public static int[] sort( int[] arr )
   {
-	return arr;
+    if ( arr.length > 1 ) {
+      int half = arr.length / 2;
+      int[] a = new int[half];
+      int[] b = new int[arr.length - half];
+
+      for ( int i = 0; i < a.length; i++ ) { //copies first half of arr to a
+        a[i] = arr[i];
+      }
+      for ( int j = 0; j < b.length; j++){ //copies second half of arr to b
+        b[j] = arr[half + j];
+      }
+      arr = merge( sort(a), sort(b) );
+    }
+    return arr;
   }//end sort()
 
 
@@ -102,8 +126,6 @@ public class MergeSort
       printArray( sort( arr5 ) );
       printArray( sort( arr6 ) );
       printArray( sort( arr7 ) );
-    /*~~~~~~~~~~~~~~ Ye Olde Tester Bar ~~~~~~~~~~~~~~
-      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   }//end main()
 
 }//end class MergeSort
