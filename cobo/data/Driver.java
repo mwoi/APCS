17700651b32ffa5d4d5ac3c4ddc82bccf844edc3
mwoi <mwu20@stuy.edu>
2022-04-06 16:53:21 -0400

    bobo

--- cobo/data/Driver.java
+++ cobo/data/Driver.java
@@ -1,14 +1,43 @@
+/*
+ * Arrays of objects
+ */
+
 import core.data.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Driver {
    public static void main(String[] args) {
-      String id1 = "peace-cable";
-      DataSource ds1 = DataSource.connect("https://www.submarinecablemap.com/submarine-cable/" + id1);
-      ds1.setCacheTimeout(15 * 60);
-      ds1.load();
-      //ds1.printUsageString();
+      DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/index.xml").load();
+      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name",
+             "station/station_id", "station/state",
+             "station/latitude", "station/longitude", "weather", "temp_f", "wind_degrees");
+      System.out.println("Total stations: " + allstns.size());
 
-
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a state abbreviation: ");
+      String state = sc.next();
+      System.out.println("Stations in " + state);
+      for (WeatherStation ws : allstns) {
+         if (ws.isLocatedInState(state)) {
+            System.out.println("  " + ws.getId() + ": " + ws.getName());
+         }
+      }
+      Scanner sc2 = new Scanner(System.in);
+      System.out.println("Enter a station ID: ");
+      String StatId = sc.next();
+      WeatherStation ws2 = null;
+      for (WeatherStation ws : allstns) {
+         if (ws.getID().equals(StatId)) {
+            ws2 = ws;
+         }
+      }
+      System.out.println("Info  for " + state);
+      System.out.println("Latitude: " + ws2.getLatitude());
+      System.out.println("Longitude: " + ws2.getLongitude());
+      System.out.println("Weather: " + ws2.getWeather());
+      System.out.println("Temperature (F): " + ws2.getTemp_f());
+      System.out.println("Wind: " + ws2.getWind());
 
    }
 }
--- cobo/data/WeatherStation.java
+++ cobo/data/WeatherStation.java
@@ -8,13 +8,20 @@ public class WeatherStation {
    private String state;
    private double lat;
    private double lng;
+   private double wind;
+   private double temp_f;
 
-   WeatherStation(String name, String id, String state, double lat, double lng) {
+
+   WeatherStation(String name, String id, String state, double lat, double lng, double wind, double temp) {
       this.name = name;
       this.id = id;
       this.lat = lat;
       this.lng = lng;
       this.state = state;
+      this.lat = lat;
+      this.lng = lng;
+      this.wind = wind;
+      this.temp_f = temp_f;
    }
 
    /* Produce the id of this station */
@@ -27,9 +34,30 @@ public class WeatherStation {
       return name;
    }
 
+   public double getLatitude() {
+      return lat;
+   }
+
+   public double getLongitude() {
+      return lng;
+   }
+
+   public double getWind() {
+      return wind;
+   }
+
+   public double getTemp_f() {
+      return temp_f;
+   }
+   public double getTemp_c() {
+      return ((temp_f - 32)*(5/9));
+   }
+
    /* Determine if this weather station is located in the given state */
    public boolean isLocatedInState(String st) {
       return this.state.equals(st);
    }
+
+
 
 }
